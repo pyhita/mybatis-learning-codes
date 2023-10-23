ac690cd1c5ba08ebd27a0f821b10352202ffa760
kante_yang <kante_yang@trendmicro.com>
2023-10-23 23:04:32 +0800

    initial commit

---
+++ mybatis-demos/src/main/java/com/yangtao/Main.java
@@ -0,0 +1,11 @@
+package com.yangtao;
+
+/**
+ * @Author: kante_yang
+ * @Date: 2023/10/20
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
